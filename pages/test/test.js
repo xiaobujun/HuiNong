870edbd60190387025f53158ded346edc8ad4714
cuteMiao213 <dy_zxp@163.com>
2022-04-18 19:34:55 +0800

    可以识别图片叶子的病毒

--- pages/test/test.js
+++ pages/test/test.js
@@ -2,6 +2,7 @@ Page({
   test(){
     var that=this;
     wx.chooseMedia({
+      // 设置读取数据的类型
       count:1,
       mediaType:['image'],
       sourceType:['album'],
@@ -9,12 +10,18 @@ Page({
       success:(res)=>{
         let url=res.tempFiles[0].tempFilePath
         wx.setStorageSync("res_imgurl", url);
+        // 照片转成base64
         wx.getFileSystemManager().readFile({
           filePath: url,
           encoding: 'base64',
           success:(res)=>{
+            // 更新数据
             let base64 = res.data;
-              that.getToken(base64);
+            that.setData({
+              base64: base64
+            })
+            // 发送数据
+            that.getToken();
           },
           fail:(res)=>{
             console.log(res);
@@ -28,38 +35,45 @@ Page({
       }
     })
   },
-  getToken(base64) {
+  getToken() {
+    let this_ = this
     // 鉴权认证
     wx.request({
       url: 'https://aip.baidubce.com/oauth/2.0/token',
       data: {
-        grant_type: 'client_credentials',
-        client_id: 'qw9jD3DnZvpHtcoT6dnc2Wjb',
-        client_secret: 'NfflGfhhxikgh3abKAYaLOutqwkqkGGc',
+        grant_type:'client_credentials',
+        client_id:'mTzozXMfAjy0i2pugklNexRX',
+        client_secret:'vSl3hpZiYX9YIX6HtfNiF7YalSYqULFB'
       },
-      success: (res) => {
+      success: res => {
+        // 设置access_token
         let token = res.data.access_token;
-        console.log('调用成功' + res.data.access_token);
-        // 调用动物识别API
+        this_.setData({
+          access_token: token
+        })
+        // console.log('调用成功' + res.data.access_token);
+        
+        // 发送请求获取扫描信息
         wx.request({
-          url: 'https://aip.baidubce.com/rpc/2.0/ai_custom/v1/classification/huinong',
+          url: 'https://aip.baidubce.com/rpc/2.0/ai_custom/v1/classification/miao213?access_token=' + this_.data.access_token,
           header: {
             "Content-type": "application/json",
           },
-          method: 'post',
-          data: {
-            image: 111,
-            access_token: token,
-            top_num: 2
+          method: 'POST',
+          data:{
+            "image": this_.data.base64,
+            "top_num": 3
           },
+          // 成功获取了数据
           success:(res)=> {
+            console.log(res)
             //console.log('识别成功跳转页面')
-            console.log(res.data.image)
-            console.log(res.data); //控制台输出识别后得到的数据中的结果
+            // console.log(res.data.image)
+            // console.log(res.data); //控制台输出识别后得到的数据中的结果
           },
           fail:(res)=>{
-            console.log(this.data.image)
-            console.log('调用错误')
+            // console.log(this.data.image)
+            console.log('没有获取到数据')
           }
         });
       }
