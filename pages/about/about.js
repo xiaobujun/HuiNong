2d8f711c8b83ff8c30e0f249f2df4407ea1174da
cutemiao <dy_zxp@163.com>
2022-06-27 20:49:30 +0800

    fix(abou、talkIssue组件)：修改符合当前要求

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,137 +1,129 @@
 // pages/about/about.js
-const app=getApp()
+const app = getApp();
 Page({
   data: {
-    about: [{
+    about: [
+      {
         id: 0,
-        icon: '../icons/个人.png',
-        title: '个人信息',
-        rightIco: '../icons/右.png'
+        icon: "../icons/个人.png",
+        title: "个人信息",
+        rightIco: "../icons/右.png",
       },
       {
         id: 1,
-        icon: '../icons/histories.png',
-        title: '浏览历史',
-        rightIco: '../icons/右.png'
+        icon: "../icons/histories.png",
+        title: "浏览历史",
+        rightIco: "../icons/右.png",
       },
       {
         id: 2,
-        icon: '../icons/send.png',
-        title: '我的发布',
-        rightIco: '../icons/右.png'
+        icon: "../icons/send.png",
+        title: "发布",
+        rightIco: "../icons/右.png",
       },
-      {
-        id: 3,
-        icon: '../icons/feedback.png',
-        title: '用户反馈',
-        rightIco: '../icons/右.png'
-      },
-      {
-        id: 4,
-        icon: '../icons/setting.png',
-        title: '设置',
-        rightIco: '../icons/右.png'
-      }
-    ]
+    ],
   },
 
-  onLoad:function() {
-    console.log('全局传过来的',app.globalData.userInfo)
+  onLoad: function () {
+    console.log("全局传过来的", app.globalData.userInfo);
     this.setData({
-      userInfo:app.globalData.userInfo
-    })
+      userInfo: app.globalData.userInfo,
+    });
   },
   /**
    * 用户登录
    */
   login() {
-    var that = this
+    var that = this;
     // 获取用户信息
     wx.getUserProfile({
-      desc: '登录',
+      desc: "登录",
       success: (res) => {
-        wx.setStorageSync('openid', app.globalData.openid)
-        console.log('成功获取到用户信息', res.userInfo)
-        var user = res.userInfo
+        wx.setStorageSync("openid", app.globalData.openid);
+        console.log("成功获取到用户信息", res.userInfo);
+        var user = res.userInfo;
         that.setData({
-          userInfo: user
-        })
+          userInfo: user,
+        });
         // 判断先前是否已经注册过
-        wx.cloud.database().collection('user').where({
-          _openid:app.globalData.openid
-        }).get({
-          success(res) {
-            console.log('成功获取到该oppenid下的所有数据', res)
-            // 如果未注册则添加到数据库
-            if (res.data.length == 0) {
-              wx.cloud.database().collection('user').add({
-                data: {
-                  avatar: user.avatar,
-                  name: user.name
-                },
-                success(res) {
-                  console.log('插入数据成功',res)
-                  wx.showToast({
-                    title: '登录成功',
-                  })
-                },
-                fail(res){
-                  console.log('失败啦')
-                }
-              })
-            }else{    // 如果已注册则更新用户信息
-              that.setData({
-                userInfo:res.data[0]
-              })
-            }
-          }
-        })
-      }
-    })
+        wx.cloud
+          .database()
+          .collection("user")
+          .where({
+            _openid: app.globalData.openid,
+          })
+          .get({
+            success(res) {
+              console.log("成功获取到该oppenid下的所有数据", res);
+              // 如果未注册则添加到数据库
+              if (res.data.length == 0) {
+                wx.cloud
+                  .database()
+                  .collection("user")
+                  .add({
+                    data: {
+                      avatar: user.avatarUrl,
+                      name: user.nickName,
+                    },
+                    success(res) {
+                      console.log("插入数据成功", res);
+                      wx.showToast({
+                        title: "登录成功",
+                      });
+                    },
+                    fail(res) {
+                      console.log("失败啦");
+                    },
+                  });
+              } else {
+                // 如果已注册则更新用户信息
+                that.setData({
+                  userInfo: res.data[0],
+                });
+              }
+            },
+          });
+      },
+    });
   },
   /**
    * 退出登录
    */
-  loginOut(){
-    app.globalData.userInfo=null
+  loginOut() {
+    app.globalData.userInfo = null;
     this.setData({
-      userInfo:null
-    })
+      userInfo: null,
+    });
   },
   /**
    * 页面跳转
-   * @param {} e 
+   * @param {} e
    */
   skip: function (e) {
     var index = e.currentTarget.dataset.index;
     console.log(index);
+
     switch (index) {
       case 0:
         wx.navigateTo({
-          url: '../personInfo/person',
-        })
+          url: "../personInfo/person",
+        });
         break;
+
       case 1:
         wx.navigateTo({
-          url: '../history/history',
-        })
+          url: "../history/history",
+        });
         break;
+
       case 2:
         wx.navigateTo({
-          url: '../issue/issue',
-        })
-        break;
-      case 3:
-        wx.navigateTo({
-          url: '../feedback/feedback',
-        })
-        break;
-        case 4:
-        wx.navigateTo({
-          url: '../setting/setting',
-        })
+          url: "pages/talkIssue/index",
+          fail: (res) => {
+            console.log(res)
+          },
+        });
         break;
     }
-  }
-
-})
+  },
+});
